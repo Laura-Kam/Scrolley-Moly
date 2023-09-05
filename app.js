796a2aa0ba5ba39306e82ae210da0d2a62a30361
Laura Kambanis <kambanislaura@gmail.com>
2023-09-05 14:05:25 +0100

    added smooth scroll, targetted sections through JS for links, after clicking on links, closed drop down links

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ navToggle.addEventListener("click", function () {
   //the class is added dynamically- it is added after links container
   //then removed. Specificity is higher for the second class as it appears later in the style sheet.
   const containerHeight = linksContainer.getBoundingClientRect().height;
-  console.log(containerHeight);
+  // console.log(containerHeight);
   const linksHeight = links.getBoundingClientRect().height;
   //height of the links in total.
 
   //links container height is 0;
 
   //you are targetting unordered list (all links- height total)
-  console.log(linksHeight);
+  // console.log(linksHeight);
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
   } else {
@@ -36,6 +36,49 @@ navToggle.addEventListener("click", function () {
   //   linksContainer.classList.toggle("show-links");
 });
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+//TopLink hidden but rendered dynamically dependent on scroll.
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  //number of pixels been scrolled vertically - 0 top - then increases.
+  const scrollHeight = window.pageYOffset;
+  //this is total height of all links in drop-down menu.
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
+
 // select links
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    //start from index of 1 (gets rid of #);
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    console.log(id);
+    const element = document.getElementById(id);
+    //how far element is from it's parent in a rel/fixed.absolute/sticky position
+    let position = element.offsetTop;
+    console.log(position);
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
